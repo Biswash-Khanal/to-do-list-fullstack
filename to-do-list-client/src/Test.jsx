2049c09ea60e10fc830c97efcb4a12332d7581a9
Biswash Khanal <kbiswash47@gmail.com>
2025-06-28 03:49:05 +0545

    setup tailwind, custom fonts and colors

---
+++ to-do-list-client/src/Test.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Test = () => {
+	return (
+		<div className="absolute top-[50vh] left-[50vh] content-center text-center justify-center text-9xl flex  bg-primary border-secondary  font-bitcount">
+			Hello World
+		</div>
+	);
+};
+
+export default Test;
