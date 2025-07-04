a28a42d4c36d0ff55edcba24aaa71190da9d3e77
Biswash Khanal <kbiswash47@gmail.com>
2025-07-04 19:56:48 +0545

    minor changes

--- to-do-list-client/src/pages/Home.jsx
+++ to-do-list-client/src/pages/Home.jsx
@@ -1,13 +1,13 @@
 
 import TodoCard from "../components/TodoCard";
-import { useAppContext } from "../context/AppContext";
+
 import { dummyTodos } from "../assets/assets.js";
 
 
 const Home = () => {
-	const {isScrolled} = useAppContext();
+	
 	return (
-		<div className={`flex flex-col items-center bg-gradient-to-b ${isScrolled?"from-secondary to-primary":"from-primary to-secondary"} py-32 gap-10`}>
+		<div className={`flex flex-col items-center bg-gradient-to-b from-secondary to-primary py-32 gap-10`}>
 			{console.log("dummyTodos:", dummyTodos)}
 
 
