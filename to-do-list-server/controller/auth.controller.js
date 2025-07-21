b3446cc6f2ccd29f7f8e50a69e4d9599672ab5ed
biswash-khanal <kbiswash47@gmail.com>
2025-07-21 19:13:04 +0545

    samesite set to none

--- to-do-list-server/controller/auth.controller.js
+++ to-do-list-server/controller/auth.controller.js
@@ -95,14 +95,14 @@ export const loginUser = async (req, res, next) => {
 			res.cookie("token", token, {
 				httpOnly: true,
 				secure: NODE_ENV === "production",
-				sameSite: "strict",
+				sameSite: "none",
 				maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 			});
 		} else {
 			res.cookie("token", token, {
 				httpOnly: true,
 				secure: NODE_ENV === "production",
-				sameSite: "strict",
+				sameSite: "none",
 			});
 		}
 
@@ -168,14 +168,14 @@ export const googleLoginUser = async (req, res, next) => {
 			res.cookie("token", jwttoken, {
 				httpOnly: true,
 				secure: NODE_ENV === "production",
-				sameSite: "strict",
+				sameSite: "none",
 				maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 			});
 		} else {
 			res.cookie("token", jwttoken, {
 				httpOnly: true,
 				secure: NODE_ENV === "production",
-				sameSite: "strict",
+				sameSite: "none",
 			});
 		}
 
